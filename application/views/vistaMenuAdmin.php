223d0e184a51b1760a8e489da93e8c512d7b6598
bperezorozco <bperezorozco@gmail.com>
2012-08-31 16:53:01 -0500

    V0.0
    31-ago-2012 16h52
    
    Incluye los primeros archivos para hacer la primera rama del proyecto.

---
+++ application/views/vistaMenuAdmin.php
@@ -0,0 +1,41 @@
+<html>
+	<head>
+		<title>Menú</title>
+		<link type="text/css" href="<?php echo base_url();?>css/cssMenu.css" rel="stylesheet"/>
+	</head>
+	<body>
+		<label for="txtBienvenido" id="txtBienvenido">¡Bienvenido, Usuario!</h1>
+		<div id="panelNotificaciones">
+			Notificaciones
+		</div>
+		<div id="logo"><img src="<?php echo base_url();?>img/logoVilla.png" height="250" width="250" /></div>
+		<div id="opcionesMenu1">
+			<div id="btnDonadores" class="circulo">
+				<div id="textoCirculo">Donadores</label>
+			</div>
+			<div id="btnDonaciones" class="circulo">
+				<div id="textoCirculo">Donaciones</label>
+			</div>
+			<div id="btnCampanias" class="circulo">
+				<div id="textoCirculo">Campañas</label>
+			</div>
+			<div id="btnNinios" class="circulo">
+				<div id="textoCirculo">Niños</label>
+			</div>
+		</div>
+		<div id="opcionesMenu2">
+			<div id="btnMailing" class="circulo">
+				<div id="textoCirculo">Mailing</label>
+			</div>
+			<div id="btnComprobantes" class="circulo">
+				<div id="textoCirculo">Comprobantes</label>
+			</div>
+			<div id="btnReportes" class="circulo">
+				<div id="textoCirculo">Reportes</label>
+			</div>
+			<div id="btnUsuarios" class="circulo">
+				<div id="textoCirculo">Usuarios</label>
+			</div>
+		</div>
+	</body>
+</html>
